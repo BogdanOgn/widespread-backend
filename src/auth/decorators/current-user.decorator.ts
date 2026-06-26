3922f9a3dd3c44499bc8689bbc349f61011589ca
BogdanOgn <bogdan.ognitsiy@yandex.ru>
2026-06-26 21:04:33 +0300

    chore(prettier): add prettier config

--- src/auth/decorators/current-user.decorator.ts
+++ src/auth/decorators/current-user.decorator.ts
@@ -5,8 +5,8 @@ import { Request } from 'express';
 import { AuthenticatedUser } from '../types/authenticated-user.interface';
 
 export const CurrentUser = createParamDecorator(
-  (_: unknown, ctx: ExecutionContext): AuthenticatedUser => {
-    const request = ctx.switchToHttp().getRequest<Request>();
-    return request.user as AuthenticatedUser;
-  },
+	(_: unknown, ctx: ExecutionContext): AuthenticatedUser => {
+		const request = ctx.switchToHttp().getRequest<Request>();
+		return request.user as AuthenticatedUser;
+	},
 );
